3649118950be5d4586beeca3797bae96849a85e3
Amruthavennapusa072005 <vennapusaamrutha48@gmail.com>
2026-02-26 16:33:58 +0530

    initial commit

---
+++ OneDrive/Desktop/mernstack/.vscode/script.js
@@ -0,0 +1,22 @@
+let obj1={
+    name:'Amrutha',
+    age:19
+}
+let obj2=obj1
+obj2.name='Lakshmi';
+console.log(obj1.name)
+if(1){
+    console.log('true')
+}
+else{
+    console.log('false')
+}
+console.log(5+'5');
+console.log(5-'5');
+console.log(5=='5');
+console.log(5==='5');
+
+
+
+let arr=[1,'new',2,'3']
+console.log(arr);
